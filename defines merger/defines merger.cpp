65236eeac0b159d5aa3e1b7adbbf1b36dade9783
neeLeifee <ilka.erkola@gmail.com>
2024-10-25 14:19:53 +0300

    v1 the whole thingy redone

--- defines merger/defines merger.cpp
+++ defines merger/defines merger.cpp
@@ -2,129 +2,82 @@
 #include <string>
 #include <fstream>
 #include <map>
-#include <algorithm>
 using namespace std;
 
+
 /*
-    TODO in that function:
-        0. make another func that will add spaces on every '=' met in the file so ther thing will work propely as i use '>>' to get values
-        1. check if there are repeated defines in the file
-        2. redo reading to be done up untill '.' to properly split defines
-        3. to check if the define_value starts with '{' - that should mean there is upcoming array
+* a) we open two files and write down mentioned defines (convert_define_txt() and main())
+* b) we check if 'to merge' defines are in the 'base' defines
+*   -) if they are -> we add the previous value to the temp var,
+                      change string after '=' to the 'to merge' one,
+*                     delete that define from 'to merge',
+*                     add comment line (--) telling prev value from the temp var
+*   -)        else -> we do nothing
+* c) we add whats left in 'to merge' to the bottom
 */
 
-void my_erase(string& str, char charToRemove) {
-    // FUNCTION that:
-    //  deletes char from string
-    str.erase(remove(str.begin(), str.end(), charToRemove), str.end());
-}
-
-void my_erase(string& str, string& str_2) {
-    // FUNCTION that:
-    //  deletes string from string
-    for (unsigned int i = 0; i < str_2.size(); i++) {
-        str.erase(remove(str.begin(), str.end(), str_2[i]), str.end());
-    }
-}
-
-void my_erase(string& str, char* charsToRemove) {
-    // FUNCTION that:
-    //  deletes string from string
-    for (unsigned int i = 0; i < strlen(charsToRemove); i++) {
-        str.erase(remove(str.begin(), str.end(), charsToRemove[i]), str.end());
-    }
-}
 
 map<string, float> convert_define_txt(string adress) {
+    /*
+    * func gets file adress,
+    * returns map<define, define_value>
+    */
 
-    // FUNCTION that:
-    //      gets file adress
-    //      opens the file
-    //      gets the first "word" by getting everything into one string untill '=' was met
-    //      get the other half of the line and sets it as a float
+    ifstream file_defines(adress);
+    string buffer = "";
 
-    ifstream file(adress); // opened a file to read
-    map<string, float> output;    // returning dictionary
-    string line = "";           // line string
-    string buffer = "";           // buffer string
-    string define = "";         // the define
-    string define_value = "";   // the define value (will be used to converted from to float during the func
+    string define = "";
+    float define_value = 0.0;
 
-    if (file.is_open()) {
-        
-        while (getline(file, line)) {
-            // here we take the line (as much lines as we can from first till the last) 
-            // and put the whole line into 'buffer'
+    map<string, float> return_defines;
 
-            my_erase(line, ',');
+    if (file_defines.is_open()) {
 
-            for (char i : line) {
-                        
-                if (i != '=') {
-                    buffer += i;
-                }
-                else {
-                    char charsToDelete{' ', };
-                    my_erase(buffer, charsToDelete);
-                    define = buffer;
-                    my_erase(line, buffer.append(" = "));
-                    define_value = line;
-                }
-            
-            }
+        while (getline(file_defines, buffer)) {
+            //cout << "Define " << buffer.substr(0, buffer.find('=')) << "\n";;
+            //cout << "Value  " << buffer.substr(buffer.find('='), buffer.length()) << "\n";;
+            // debug thingy
 
-            output[define] = stof(define_value);
+            define = buffer.substr(0, buffer.find('='));
+            define.erase(remove(define.begin(), define.end(), ' '), define.end());  // removing all ' 's in the define
+            define_value = stof(buffer.substr(buffer.find('=') + 1, buffer.length()));
 
-            line.clear();
-            buffer.clear();
+            return_defines[define] = define_value;
+
             define.clear();
-            define_value.clear();
 
         }
 
     }
-    //for (const auto& pair : output)
-    //    cout << '[' << pair.first << "] = " << pair.second << "; " << endl;
 
-    return output;
+    return (return_defines);
 }
-
 
 int main() {
     map<string, float> defines_base = convert_define_txt("defines_base.lua");
 
     map<string, float> defines_from = convert_define_txt("defines_from.lua");
-    
+
     ofstream defines_txt("defines_output.lua");
 
     string buffer = "";
 
+    for (auto pair1 : defines_base) {
 
-    cout << "WAS :" << endl;
-    for (const auto& pair : defines_base)
-        cout << '[' << pair.first << "] = " << pair.second << "; " << endl;
-    cout << endl;
-    for (const auto& pair : defines_from)
-        cout << '[' << pair.first << "] = " << pair.second << "; " << endl;
+        for (auto pair2 : defines_from) {
 
-    for (const auto& pair1 : defines_base) {
-        
-        for (const auto& pair2 : defines_from) {
-        
             if (pair1.first != pair2.first) {
-                defines_txt << pair1.first + " = " + to_string(pair1.second) << endl;
+                defines_txt << pair1.first << " = " << pair1.second << ',' << endl;
             }
             else {
-                defines_txt << pair1.first << " = " << to_string(pair2.second) << endl 
-                            << "-- previously was '" << pair1.first << " = " << pair1.second << "'. This change was made by Leifee's HOI4 define merger." << endl;
+                defines_txt << pair1.first << " = " << pair2.second << ',' << endl;
+                defines_txt << "-- previously was '" << pair1.first << " = " << pair1.second << "'. This change was made by Leifee's HOI4 define merger." << endl;
             }
 
-            //cout << '[' << pair_2.first << "] = " << pair_2.second << "; " << endl;
-        
         }
 
     }
-    
+
     cout << "end" << endl;
 
 }
